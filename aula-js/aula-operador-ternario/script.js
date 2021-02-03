e08d83e41f8d0344dbbe415434b72d9c9080011f
Mlluiz39 <mlluizpereira39@gmail.com>
2021-02-03 19:24:29 -0300

    aula de ternarios

---
+++ aula-js/aula-operador-ternario/script.js
@@ -0,0 +1,17 @@
+ var idade = 17
+
+ idade >= 18 ? console.log('pode') : console.log('não pode')
+
+
+var idade2 = 17
+
+var pode = idade2 >= 18 ? true : false
+
+console.log(pode)
+
+
+var idade3 = 18
+
+var bebida = idade3 >= 18 ? 'pode' : 'não pode'
+
+console.log(bebida)
