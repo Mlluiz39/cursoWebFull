323ad7cc9240c2c389416d337d2afe0452ac5280
Mlluiz39 <mlluizpereira39@gmail.com>
2021-02-05 18:10:19 -0300

    aula de transformaçao de int e float

---
+++ aula-js/aula-transformando-number-string/script.js
@@ -0,0 +1,27 @@
+var a = '3'
+
+var b = '6'
+
+var c = a + b
+
+console.log(c)
+
+var a = '3'
+
+var b = '6'
+
+var c = parseInt(a) + parseInt(b)
+
+console.log(c)
+
+
+var a = '3.5'
+
+var b = '6'
+
+var c = parseFloat(a) + parseInt(b)
+
+console.log(c)
+
+
+
