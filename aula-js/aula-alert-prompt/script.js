b2e99dff00643eaa76d65c2c06f649721375741a
Mlluiz39 <mlluizpereira39@gmail.com>
2021-02-05 18:39:41 -0300

    aula de alert e prompt

---
+++ aula-js/aula-alert-prompt/script.js
@@ -0,0 +1,26 @@
+/*
+alert('Ola Mundo')
+
+alert('Ola Marcelo')
+
+var nome = prompt('Qual seu nome ?')
+
+alert('Bom dia ' + nome)
+
+// alert(`Bom dia ${nome}`)
+
+var numero = prompt('digite um numero')
+
+var dobro = numero + numero
+
+alert('O dobro de ' + numero + ' é ' + dobro)
+*/
+
+
+var numero2 = parseInt(prompt('digite um numero'))
+
+var dobro2 = numero2 + numero2
+
+alert('O dobro de ' + numero2 + ' é ' + dobro2)
+
+alert(`o dobro de ${numero2} é ${dobro2}`)  // template string
