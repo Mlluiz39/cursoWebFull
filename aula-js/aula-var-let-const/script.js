e5e2505905abd4cf394e2163fdb02301794e6555
Mlluiz39 <mlluizpereira39@gmail.com>
2021-02-08 21:52:43 -0300

    aula sobre variaveis do tipo var let e const

---
+++ aula-js/aula-var-let-const/script.js
@@ -0,0 +1,17 @@
+var numero1 = 4
+let numero2 = 5
+//const numero3 = 6  // não pode atribuir outro valor mas podemos mudar o valor da nova constante 
+const numero3 = [7]  // porem pode ser alterada a constante com o metodo "push" estou alterando
+
+const numero4 = {}  // com objeto tambem podemos mudar o valor da nova constante
+numero4.nome = "Marcelo"
+
+numero3.push(8)
+
+
+
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+console.log(numero4)
