97ad4ddb5e3140e5f3d4b410d2e9552006ddb972
Mlluiz39 <mlluizpereira39@gmail.com>
2021-02-17 19:41:17 -0300

    aula acessando elementos

---
+++ aula-js/js-inter-acessando elem/script.js
@@ -0,0 +1,24 @@
+
+document.getElementById("th")
+console.log(th)
+
+document.getElementById("p1")
+console.log(p1)
+
+let tp = document.getElementsByClassName("paragrafo")
+console.log(tp)
+
+let paragrafos = document.getElementsByClassName("paragrafo")
+console.log(paragrafos)
+
+for (let p of paragrafos){}
+for(let p of paragrafos){console.log(p)}
+
+let th2 = document.getElementsByTagName("h1")
+console.log(th2)
+console.log(th2[0])
+
+let tp2 = document.getElementsByTagName("p")
+console.log(tp2[0])
+console.log(tp2[1])
+console.log(tp2[2])
